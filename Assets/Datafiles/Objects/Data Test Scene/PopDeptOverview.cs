17e88af188d6f9fe107af2f0b6c13ca101844f19
c1645601 <BurgeKJ@Cardiff.ac.uk>
2018-11-30 12:20:31 +0000

    Added PopDeptOverview script and resolved merge errors

---
+++ Assets/Datafiles/Objects/Data Test Scene/PopDeptOverview.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopDeptOverview : MonoBehaviour {
+
+	public Font font;	
+
+	// Use this for initialization
+	void Start () {
+
+		//
+		GameObject dept = GameObject.Find("Marketing");
+		GameObject deptOverview = dept.transform.Find("DeptOverview").gameObject;
+		GameObject topContainer = deptOverview.transform.Find("Top Container").gameObject;
+		GameObject title = topContainer.transform.Find("Dept Title").gameObject;
+		//
+		title.transform.SetParent(topContainer.transform);
+		//
+		Text myText = title.AddComponent<Text>();
+		myText.font = font;
+		myText.text = "Title Test";
+		myText.alignment = TextAnchor.MiddleCenter;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
